9d342d10d9d7ed632f60a22f0fb32b7a5ca5d801
Noman <noman5456318@gmail.com>
2023-08-30 00:22:44 +0500

    file added

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,24 +5,35 @@ import './App.css'
 
 function App() {
 
-let counter = 0;
+// let counter = 0;  q k ab hum state ki madad se update karenge 
 
-const updateCounter =()=>{
+let [counter, setCounter] = useState(0)
+
+const incrimentCounter =()=>{
   // console.log("clicked")
   // console.log("Clicked" , Math.random())
 
-  counter = counter +1 ;
-  console.log("Updated", counter)
+  // counter = counter +1 ;
+  // console.log("Updated", counter)
 
+
+  
   // yahan par actual men counter update to ho rah he consol mn show bgi kar rha he par wo ui ko update nhi kar rha he isi wajh se hooks ka concept aya k ap ui ko update kar sakte hen 
+
+  setCounter(counter + 1)
+}
+
+const decrimentCounter = ()=>{
+  setCounter(counter-1)
 }
 
   return (
     <div className='app'>
      <h1>React Counter</h1>
 
-     <button onClick={updateCounter}>Incriment{counter}</button>
-     <button>Decrement{counter}</button>
+     <button onClick={incrimentCounter}>Incriment</button>
+     <h2>{counter}</h2>
+     <button onClick={decrimentCounter}>Decrement</button>
     </div>
   )
 }
