c34acd6102de44730ff9e2927481113c495d5e5a
Noman <noman5456318@gmail.com>
2023-08-29 23:49:13 +0500

    file updated

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -8,8 +8,15 @@ function App() {
 let counter = 0;
 
 const updateCounter =()=>{
-  console.log("clicked")
+  // console.log("clicked")
+  // console.log("Clicked" , Math.random())
+
+  counter = counter +1 ;
+  console.log("Updated", counter)
+
+  // yahan par actual men counter update to ho rah he consol mn show bgi kar rha he par wo ui ko update nhi kar rha he isi wajh se hooks ka concept aya k ap ui ko update kar sakte hen 
 }
+
   return (
     <div className='app'>
      <h1>React Counter</h1>
