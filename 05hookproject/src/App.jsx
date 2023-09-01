cf08745b59858f7cce7600f6b6812ad39ac66274
Noman <noman5456318@gmail.com>
2023-09-01 15:54:31 +0500

    project updated

--- 05hookproject/src/App.jsx
+++ 05hookproject/src/App.jsx
@@ -1,10 +1,40 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 
 function App() {
 
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [characterAllowed, setCharacterAllowed] = useState(false)
+  const [password, setPassword] = useState("")
+
+
+  const passwordGenerator = useCallback(()=>{
+
+    let pass = ""
+    let str= "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) {
+      str +="1234567890"
+    }
+    if (characterAllowed) {
+      str +="!@#$%^&*-_+=[]{}~`"
+    }
+    for (let i = 0; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1)
+      
+      pass = str.charAt(char)
+    }
+
+    setPassword(pass)
+   
+  }
+  
+  
+  
+  ,[length,numberAllowed,characterAllowed,password])
 
   return (
     <div>
