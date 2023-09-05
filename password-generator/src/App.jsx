bfb3d54f5dc0524f9dda320077b3422dbcac7eb9
Noman <noman5456318@gmail.com>
2023-09-05 23:12:09 +0500

    ui completed

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -6,13 +6,34 @@ function App() {
 
 
   return (
-    <div>
-      <div className="bg-img">
+    <div className="hero">
+    <div className="content">
+      <h1>
+        PASSWORD <span>GENERATOR</span>
+      </h1>
+      <div className="form">
+        <input type="text" placeholder="Your password" readOnly />
+        <button className="btn">Copy</button>
+      </div>
 
-        
-       
+      <div className="rules">
+        <div className="items">
+          <input type="range" />
+          <label>Length</label>
+        </div>
+
+        <div className="items">
+          <input className="checks" type="checkbox" />
+          <label>Numbers</label>
+        </div>
+
+        <div className="items">
+          <input className="checks" type="checkbox" />
+          <label>Characters</label>
+        </div>
       </div>
     </div>
+  </div>
   )
 }
 
