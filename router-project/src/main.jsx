82659e2d87a3570f5c3df58ff4862ca8b46c2007
Noman <noman5456318@gmail.com>
2023-09-12 16:31:32 +0500

    frontend work completed

--- router-project/src/main.jsx
+++ router-project/src/main.jsx
@@ -2,9 +2,52 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+
+
+//  method 1 ----complex method
+
+// const router = createBrowserRouter([
+//   {
+//     path : '/',
+//     element: <Layout/>,
+//     children:[
+//       {
+//         path:"",
+//         element:<Home/>
+//       },
+//       {
+//         path: "about",
+//         element:<About/>
+//       },
+//       {
+//         path: "contact",
+//         element:<Contact/>
+//       }
+//     ]
+//   }
+// ])
+
+// method 2 ---- easy method
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>} >
+      <Route path='' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      
+    </Route>
+  )
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider  router={router} />
   </React.StrictMode>,
 )
