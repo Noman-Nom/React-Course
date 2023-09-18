2ebff8487c47d8a6c0e998800420b5ee1771b66f
Noman <noman5456318@gmail.com>
2023-09-19 00:20:12 +0500

    page sizing added

--- newsApi-project/src/components/News/News.jsx
+++ newsApi-project/src/components/News/News.jsx
@@ -6,6 +6,7 @@ const News = () => {
     //  variables 
     const ApiKey = 'bcd2aeba084a4b89b74f151b9d2bd920';
     const catogery = "general"
+    const pageSize = 9
   
 
 
@@ -19,7 +20,7 @@ const News = () => {
         const searchNews = async () => {
             try {
              
-                const response = await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2023-08-18&sortBy=publishedAt&catogery=${catogery}&apiKey=${ApiKey}`)
+                const response = await fetch(`https://newsapi.org/v2/everything?q=apple&from=2023-08-18&sortBy=publishedAt&catogery=${catogery}&pageSize=${pageSize}&apiKey=${ApiKey}`)
                 const data = await response.json()
                 setNewsData(data.articles)
                 console.log(data)
