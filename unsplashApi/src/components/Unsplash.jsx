9bf0dea24709d04b734beb6bc2cee8d26d52f541
Noman <noman5456318@gmail.com>
2023-09-18 18:44:09 +0500

    setup completed

--- unsplashApi/src/components/Unsplash.jsx
+++ unsplashApi/src/components/Unsplash.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Unsplash.css'
 
+import { useState } from 'react'
+
 const Unsplash = () => {
+
+  const [inputData, setInputData] = useState("")
+  // console.log(inputData)
   return (
-    <div>Unsplash</div>
+    <div className="container">
+      <h1>Search Your Favorate Images</h1>
+     <form action="">
+      <input type="text" value={inputData} id='search-input' placeholder='Search images...'
+        onChange={(e)=>setInputData(e.target.value)}
+      />
+
+     </form>
+
+    </div>
   )
 }
 
