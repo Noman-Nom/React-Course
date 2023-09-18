2a30166d116ad6c916658265da95290daea65b1b
Noman <noman5456318@gmail.com>
2023-09-18 22:38:44 +0500

    setup completed

--- newsApi-project/src/App.jsx
+++ newsApi-project/src/App.jsx
@@ -1,34 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import News from './components/News/News'
+
+{/* https://newsapi.org/v2/everything?q=tesla&from=2023-08-18&sortBy=publishedAt&catogery=general&apiKey=bcd2aeba084a4b89b74f151b9d2bd920 */}
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+ <div>
+      <h1>Touch With The Best News App</h1>
+      <News/>
+ </div>
   )
 }
 
