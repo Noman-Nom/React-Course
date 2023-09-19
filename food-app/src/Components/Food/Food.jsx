bde392585dcab2f96d9e5e990a6e0ee25601bae6
Noman <noman5456318@gmail.com>
2023-09-19 20:51:09 +0500

    project completed

--- food-app/src/Components/Food/Food.jsx
+++ food-app/src/Components/Food/Food.jsx
@@ -3,63 +3,78 @@ import { useState } from 'react'
 import './Food.css'
 
 const Food = () => {
-// use states.......................................
-const [data, setData] = useState([])
-const [inputData, setInputData] = useState('')
+    // use states.......................................
+    const [data, setData] = useState([])
+    const [inputData, setInputData] = useState('')
 
-// use effects......................................
-
-
-const getData = async ()=>{
-            const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputData}`
-             const response = await fetch(url)
-             const data = await response.json()
-            //  console.log(data)
-            setData(data.meals)
-            console.log(data.meals)
-
-}
+    // use effects......................................
+    useEffect(() => {
+        getStaticData()
+    }, [])
 
 
-// functions.....................................
 
-const  handleClick = ()=>{
+
+    // functions.....................................
+
+    const getStaticData = async () => {
+        // static data 
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=egg`
+        const response = await fetch(url)
+        const data = await response.json()
+
+        setData(data.meals)
+
+    }
+
+
+    const getData = async () => {
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputData}`
+        const response = await fetch(url)
+        const data = await response.json()
+        //  console.log(data)
+        setData(data.meals)
+        // console.log(data.meals)
+
+    }
+
+    const handleClick = () => {
         // console.log(inputData)
         getData()
         setInputData('')
-}
+    }
 
 
-  return (
-    <div className="container">
+    return (
+        <div className="container">
 
-        <div className="input-box">
-            <input type="text" value={inputData} onChange={(e)=>setInputData(e.target.value)} />
-            <button onClick={handleClick}>Search</button>
+            <div className="input-box">
+                <input type="text" value={inputData} onChange={(e) => setInputData(e.target.value)} />
+                <button onClick={handleClick}>Search</button>
+            </div>
+            <div className="food-container">
+                <h2>Your Search Food:</h2>
+
+                <div className="row" >
+                    {
+                        data.map((meal) => (
+
+                            <div className="col" key={meal.idMeal
+                            }  >
+                                <img src={meal.strMealThumb} alt="" />
+                                <h4>{meal.strMeal}</h4>
+                                <a href="#">{meal.strMeal}</a>
+                            </div>
+
+                        ))
+                    }
+                </div>
+
+            </div>
+
         </div>
-                   <div className="food-container">
-                   <h2>Your Search Food:</h2>
 
-                   <div className="row" >
-                        {
-                            data.map((meal)=>(
-                                
-                                <div className="col" key={meal.idMeal
-                                }  >
-                                  <img src={meal.strMealThumb} alt="" />
-                                  <h4>{meal.strMeal}</h4>
-                                  <a href="#">{meal.strMeal}</a>
-                                </div>
-
-))
-}
-</div>
-
-                   </div>
-                     
-    </div>
-                
-  )
+    )
 }
 
 export default Food
