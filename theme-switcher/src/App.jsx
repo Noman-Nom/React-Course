743bc7d86513e2cdf8c9ff30faa53a8bb5fbb020
Noman <noman5456318@gmail.com>
2023-09-23 00:45:48 +0500

    working started

--- theme-switcher/src/App.jsx
+++ theme-switcher/src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react"
+import { ThemeProvider } from "./Contexts/Theme"
 
 
 function App() {
 
 
   return (
-    <div>
-     
+    
+
+    <ThemeProvider value={{themeMode,darkMode,lightMode}}  >
+<div className="flex flex-wrap min-h-screen items-center">
+    <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+            
+        </div>
     </div>
+</div>
+</ThemeProvider>
   )
 }
 
