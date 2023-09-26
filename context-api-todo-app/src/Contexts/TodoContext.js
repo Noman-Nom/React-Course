5039364d960d4f8f53828829e4b859fc205cbdfe
Noman <noman5456318@gmail.com>
2023-09-27 01:23:20 +0500

    functionality started

--- context-api-todo-app/src/Contexts/TodoContext.js
+++ context-api-todo-app/src/Contexts/TodoContext.js
@@ -12,7 +12,7 @@ export const TodoContext = createContext({
 
     addTodo: (todo)=>{},
     updateTodo: (id,todo)=>{},
-    deleteTod: (id)=>{},
+    deleteTodo: (id)=>{},
     toggleCompleted: (id)=>{}
 })
 
