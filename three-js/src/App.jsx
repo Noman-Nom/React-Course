5c561511e302c71a4fe800212167619743c52c4a
Noman <noman5456318@gmail.com>
2023-10-12 00:14:44 +0500

    changes

--- three-js/src/App.jsx
+++ three-js/src/App.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import './App.css'
 
 import * as THREE from 'three';
 import { Canvas, useFrame } from '@react-three/fiber'
 import CircleImg from './assets/circle.png'
+import { Suspense } from 'react';
 
 const Points = ()=>{
+
+  <Points>
+
+    <bufferGeometry attach="geometry"> 
+
+    </bufferGeometry>
+
+    <pointsMaterial  attach='meterial'/>
+  </Points>
 
 }
 
 const AnimationCanvas= ()=>{
 
   return(
-    <Canvas>
+    <Canvas
+    colorManagement={false}
+    camera={{position:[100,10,0],fov:75}}
+    >
+          <Suspense  fallback={null}>
+
+      <Points/>
+          </Suspense>
 
       
     </Canvas>
@@ -24,7 +41,11 @@ const App = () => {
   return (
     <div className='anim'>
 
+      <Suspense fallback={<div>Loading.....</div>}>
       <AnimationCanvas/>
+
+      </Suspense>
+
     </div>
   )
 }
