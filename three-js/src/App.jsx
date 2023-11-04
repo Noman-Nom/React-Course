7f28ec41513146656c4224ff1849fac6275db5c6
NomiTechh <muhammadnoman.ned@gmail.com>
2023-11-04 14:05:03 +0500

    new project

--- three-js/src/App.jsx
+++ three-js/src/App.jsx
@@ -1,65 +1,77 @@
-import React, { Suspense } from 'react'
-import './App.css'
+import React, { Suspense, useMemo } from "react";
+import "./App.css";
 
-import * as THREE from 'three';
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import CircleImg from './assets/circle.png'
-import { Suspense } from 'react';
+import * as THREE from "three";
+import { Canvas, useLoader } from "@react-three/fiber";
+import CircleImg from "./assets/circle.png";
 
+const Points = () => {
+  const imgTex = useLoader(THREE.TextureLoader, CircleImg);
 
-const Points = ()=>{
-  const imgTex = useLoader(THREE.TextureLoader,CircleImg);
+  const count = 100;
+  const sep = 3;
+  const positions = useMemo(() => {
+    const positions = [];
 
+    for (let xi = 0; xi < count; xi++) {
+      for (let zi = 0; zi < count; zi++) {
+        const x = sep * (xi - count / 2);
+        const z = sep * (zi - count / 2);
+        const y = 0;
+        positions.push(x, y, z);
+      }
+    }
 
-  <Points>
+    return new Float32Array(positions);
+  }, [count, sep]);
 
-    <bufferGeometry attach="geometry"> 
+  return (
+    <>
+      <points>
+        <bufferGeometry attach="geometry">
+          <bufferAttribute
+            attachObject={["attributes", "position"]}
+            array={positions}
+            itemSize={3}
+          />
+        </bufferGeometry>
 
-    </bufferGeometry>
+        <pointsMaterial
+          attach="material"
+          map={imgTex}
+          color={0x00aaff}
+          size={0.5}
+          sizeAttenuation
+          transparent={false}
+          alphaTest={0.5}
+          opacity={1.0}
+        />
+      </points>
+    </>
+  );
+};
 
-    <pointsMaterial  
-    attach='meterial'
-    map={imgTex}
-    color={0x00AAFF}
-    size={0.5}
-    sizeAttenuation
-    transparent={false}
-    alphaTest={0.5}
-    opacity={1.0}
-    />
-  </Points>
-
-}
-
-const AnimationCanvas= ()=>{
-
-  return(
+const AnimationCanvas = () => {
+  return (
     <Canvas
-    colorManagement={false}
-    camera={{position:[100,10,0],fov:75}}
+      colorManagement={false}
+      camera={{ position: [100, 10, 0], fov: 75 }}
     >
-          <Suspense  fallback={null}>
-
-      <Points/>
-          </Suspense>
-
-      
+      <Suspense fallback={null}>
+        <Points />
+      </Suspense>
     </Canvas>
-  )
-}
-
+  );
+};
 
 const App = () => {
   return (
-    <div className='anim'>
-
-      <Suspense fallback={<div>Loading.....</div>}>
-      <AnimationCanvas/>
-
+    <div className="anim">
+      <Suspense fallback={<div>Loading...</div>}>
+        <AnimationCanvas />
       </Suspense>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
