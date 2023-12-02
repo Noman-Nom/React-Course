22a133b54a49c305e88dcc045442b19f6066312c
Noman-Nom <noman5456318@gmail.com>
2023-12-02 20:23:19 +0500

    project setup

--- disneyclone/src/Constants/images.js
+++ disneyclone/src/Constants/images.js
@@ -17,3 +17,64 @@ import slider1 from '../Assets/slider-badag.jpg'
 import slider2 from '../Assets/slider-badging.jpg'
 import slider3 from '../Assets/slider-scale.jpg'
 import slider4 from '../Assets/slider-scales.jpg'
+import viewers1 from '../Assets/viewers-disney.png'
+import viewers2 from '../Assets/viewers-marvel.png'
+import viewers3 from '../Assets/viewers-national.png'
+import viewers4 from '../Assets/viewers-pixar.png'
+import viewers5 from '../Assets/viewers-starwars.png'
+
+
+// poster and cards
+
+import card1 from '../Assets/card 1.webp'
+import card2 from '../Assets/card 2.webp'
+import card3 from '../Assets/card 3.webp'
+import card4 from '../Assets/card 4.webp'
+import card5 from '../Assets/card 5.webp'
+import card6 from '../Assets/card 6.webp'
+import card7 from '../Assets/card 7.webp'
+import card8 from '../Assets/card 8.webp'
+import card9 from '../Assets/card 9.webp'
+import card10 from '../Assets/card 10.webp'
+import card11 from '../Assets/card 11.webp'
+import card12 from '../Assets/card 12.webp'
+import card13 from '../Assets/card 13.webp'
+import card14 from '../Assets/card 14.webp'
+import card15 from '../Assets/card 15.webp'
+import card16 from '../Assets/card 16.webp'
+import card17 from '../Assets/card 17.webp'
+import card18 from '../Assets/card 18.webp'
+import card19 from '../Assets/card 19.webp'
+import card20 from '../Assets/card 20.webp'
+
+
+
+
+
+export default {
+
+    logo1,
+    logo2,
+    favicon,
+    groupIcon,
+    homeBackground,
+    homeIcon,
+    loginBackground,
+    logo,
+    movieIcon,
+    originalIcon,
+    playIconBlack,
+    playIconWhite,
+    react,
+    searchIcon,
+    seriesIcon,
+    slider1,
+    slider2,
+    slider3,
+    slider4,
+    viewers1,
+    viewers2,
+    viewers3,
+    viewers4,
+    viewers5,
+}
